f296d4575f4d6912de8f3d594194d45a0825f665
Vaganuki <hugo.warnotte@gmail.com>
2025-03-17 09:22:42 +0100

    j'ai modifié le chemin vers le public je crois, y a le weekend qui est passé là

--- src/app.js
+++ src/app.js
@@ -9,7 +9,7 @@ db.sequelize.sync();
 app.set('view engine', 'ejs');
 app.set('views', './src/views');
 
-app.use(express.static('public'));
+app.use(express.static('./src/public'));
 app.use(express.json());
 app.use(router);
 
